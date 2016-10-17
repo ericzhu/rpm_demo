b9d3ce7146290c8a7cc9ed299706dbb3d92329da
Eric Zhu <eric.zhu@esignlive.com>
2016-10-17 09:39:34 -0400

    change shell script language to package.sh

--- package.sh
+++ package.sh
@@ -21,15 +21,22 @@ echo "Template file    : $SPEC_TEMPLATE_FILE"
 
 rm $SPEC_FILE_NAME
 
-# Create rpm build folder
-mkdir rpmbuild
 
-# Enter temporary folder, and create sub folder structures
+# Collect the artifacts (ears) and package them into a zip file
+
+
+
+# Create rpm build directories
+mkdir rpmbuild
 cd rpmbuild
 mkdir BUILD RPMS SOURCES SPECS SRPMS
 cd ..
 
-# Create specification file.
+# Create specification file from specification template
 sed -e "s/\${VERSION}/1-TEST-VERSION/" -e "s/\${word}/dog/" $SPEC_TEMPLATE_FILE_NAME >$SPEC_FILE_NAME
 
 tar -zcvf evm.tar.gz evm/
+
+# Copy zip file to build folder
+
+# Run build command to generate the RPM
